42810cf05d2ca4fcb2eba9e2912990815b7f0bd2
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-05-31 10:50:07 +0200

    added missing var and usage example to main

--- src/main.py
+++ src/main.py
@@ -20,4 +20,8 @@ app = create_app()
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
+    #app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
+    from delta import DeltaClient
+    from utils.config import DELTA_CERT_BASE64, DELTA_CERT_PASS, DELTA_BASE_URL, DELTA_TOP_ADM_UNIT_UUID
+    dc = DeltaClient(cert_base64=DELTA_CERT_BASE64, cert_pass=DELTA_CERT_PASS, base_url=DELTA_BASE_URL, top_adm_unit_uuid=DELTA_TOP_ADM_UNIT_UUID)
+    print(cd.get_adm_unit_list()) #NB: takes almost 5 minutes to run
--- src/utils/config.py
+++ src/utils/config.py
@@ -10,6 +10,7 @@ DEBUG = os.getenv('DEBUG', 'False') in ['True', 'true']
 PORT = os.getenv('PORT', '8080')
 POD_NAME = os.getenv('POD_NAME', 'Pod name not set')
 
+DELTA_TOP_ADM_UNIT_UUID = "c16de869-8639-4d94-aa30-fa6c8e2459b3"
 DELTA_CERT_BASE64 = os.environ['DELTA_CERT_BASE64']
 DELTA_CERT_PASS = os.environ['DELTA_CERT_PASS']
 DELTA_BASE_URL = os.environ['DELTA_BASE_URL']
