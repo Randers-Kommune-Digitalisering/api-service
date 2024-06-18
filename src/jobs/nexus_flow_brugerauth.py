94be8c0761a1ffba1beda9ee2980851e91226e7f
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-06-18 10:23:17 +0200

    lint fix

--- src/jobs/nexus_flow_brugerauth.py
+++ src/jobs/nexus_flow_brugerauth.py
@@ -29,7 +29,7 @@ def execute_brugerauth(active_org_list: list, primary_identifier: str, input_org
 
     # Get all assigned organisations for professional as list of dicts - [0] being id, [1] being uuid
     professional_org_list = _fetch_professional_org_syncIds(professional)
-    #logger.info(f"Professional current organisation: {professional_org_list}")
+    # logger.info(f"Professional current organisation: {professional_org_list}")
 
     # uuids from active_org_list not found in input_organisation_uuid_list
     unassigned_organisation_ids = [item['id'] for item in active_org_list if item['sync_id'] in input_organisation_uuid_list]
@@ -44,12 +44,11 @@ def execute_brugerauth(active_org_list: list, primary_identifier: str, input_org
 
     # Remove duplicates
     unassigned_organisation_ids = list(set(unassigned_organisation_ids))
-    #logger.info(f"Professional unassigned organisation: {unassigned_organisation_ids}")
+    # logger.info(f"Professional unassigned organisation: {unassigned_organisation_ids}")
 
     try:
         # Update the organisations for the professional
-        #_update_professional_organisations(professional, unassigned_organisation_ids)
-        if  _update_professional_organisations(professional, unassigned_organisation_ids):
+        if _update_professional_organisations(professional, unassigned_organisation_ids):
             logger.info(f'Professional {primary_identifier} updated with organisations')
 
         # Gte top organisation's supplier
@@ -63,7 +62,6 @@ def execute_brugerauth(active_org_list: list, primary_identifier: str, input_org
         logger.info(f'Professional {primary_identifier} updated')
     except Exception as e:
         logger.error(f'Failed to update professional {primary_identifier}: {e}')
- 
 
 
 def _fetch_professional(primary_identifier):
@@ -101,9 +99,10 @@ def _update_professional_supplier(professional, supplier):
     request = NexusRequest(input_response=professional, link_href="self", method="GET")
     professional_self = execute_nexus_flow([request])
 
+    # Professional configuration
     request = NexusRequest(input_response=professional_self, link_href="configuration", method="GET")
     professional_config = execute_nexus_flow([request])
-    
+
     # Only update supplier if it is None/null
     if not professional_config.get('defaultOrganizationSupplier'):
         professional_config['defaultOrganizationSupplier'] = supplier
@@ -168,7 +167,7 @@ def _collect_syncIds_and_ids_from_org(org: object):
     sync_ids_and_ids = []
     if isinstance(org, dict):
         if 'syncId' in org and org['syncId'] is not None:
-            sync_ids_and_ids.append({'id': org['id'], 'sync_id': org['syncId']}) #(org['id'], org['syncId']))
+            sync_ids_and_ids.append({'id': org['id'], 'sync_id': org['syncId']})
         for child in org.get('children', []):
             sync_ids_and_ids.extend(_collect_syncIds_and_ids_from_org(child))
     else:
@@ -176,13 +175,8 @@ def _collect_syncIds_and_ids_from_org(org: object):
     return sync_ids_and_ids
 
 
-def _add_supplier_ids(organisation_ids: list, suppliers:list):
+def _add_supplier_ids(organisation_ids: list, suppliers: list):
     for org in organisation_ids:
         supplier = next((item for item in suppliers if item.get('organizationId') == org['id']), None)
         org['supplier'] = supplier
     return organisation_ids
-
-# Example usage
-# if __name__ == "__main__":
-#    update_nexus_organisation_list()
-#    execute_brugerauth("dqb1029", ['bb29e529-06a8-47e7-abe7-8fab6824dbf9'])
